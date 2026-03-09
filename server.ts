eb547996d0108a91f29d09f4ebdf4496822a5ea8
giansambas <156150363+giansambas@users.noreply.github.com>
2026-03-10 03:58:10 +0800

    Update server.ts

--- server.ts
+++ server.ts
@@ -135,9 +135,7 @@ async function startServer() {
     });
   }
 
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+ export default app;
 }
 
 startServer();
